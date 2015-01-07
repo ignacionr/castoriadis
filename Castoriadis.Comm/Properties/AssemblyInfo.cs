7b5518b9093e5db361f161362f4221fd845f3319
Ignacio Rodriguez <ignacionr@gmail.com>
2015-01-08 01:32:12 +0700

    Arranging packages to work with myget

--- Castoriadis.Comm/Properties/AssemblyInfo.cs
+++ Castoriadis.Comm/Properties/AssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle ("Castoriadis.Comm")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("Communications (0MQ) support for Castoriadis Microservices Framework")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("Ignacio Rodriguez")]
 [assembly: AssemblyProduct ("")]
 [assembly: AssemblyCopyright ("Ignacio Rodriguez")]
 [assembly: AssemblyTrademark ("")]
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.3.1.*")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
